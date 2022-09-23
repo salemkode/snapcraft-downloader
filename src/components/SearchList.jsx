1229ab0bd052d21ff45383889013452a05d0d0d9
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2022-09-23 20:28:08 +0300

    Make search list bigger

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -3,17 +3,17 @@ function SearchItem(props) {
   return (
     <button
       type="button"
-      className="list-group-item list-group-item-action"
+      className="list-group-item py-3 list-group-item-action d-flex align-items-center"
       onClick={() => props.onClick(props.item.name)}
     >
       <img
         src={props.item.icon}
         alt=""
-        className="me-2"
-        width="40"
-        height="40"
+        className="me-4"
+        width="50"
+        height="50"
       />
-      {props.item.title}
+      <h4 className="mb-0">{props.item.title}</h4>
     </button>
   );
 }
@@ -21,7 +21,7 @@ function SearchItem(props) {
 export default function searchList(props) {
   return (
     <div className="container">
-      <div className="list-group">
+      <div className="list-group mb-5">
         {props.list.map((item) => (
           <SearchItem
             item={item}
